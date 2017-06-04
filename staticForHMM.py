944a5abb1b941db61d3eb3f3cdebc5976a1775c1
colipso <colipso@outlook.com>
2017-06-04 19:19:48 +0800

    add static for hmm

---
+++ staticForHMM.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  4 19:17:14 2017
+
+@author: hp
+"""
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import os
+
