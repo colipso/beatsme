be92290d5fa72cc6a62d89a0ccab311c17935bb3
colipso <colipso@outlook.com>
2017-07-06 21:07:09 +0800

    Add right word cut example and calc accuracy rate

--- test/test_wordCut.py
+++ test/test_wordCut.py
@@ -19,9 +19,58 @@ cut = HMM().cut_DAG_NO_HMM
 f = codecs.open("thai_new.txt",'r','utf-8')
 resultf = codecs.open('thai_new_cut_result.txt' , 'w' , 'utf-8')
 
+cut_result = []
+
 for line in f.readlines():
-    for r in cut(line):
-        resultf.write( r+'\n ')
+    cleanLine = line.strip()
+    if cleanLine != '':
+        for r in cut(cleanLine):
+            resultf.write( r+'\n ')
+            cut_result.append(r)
      
 f.close()
 resultf.close()
+
+
+#right cut
+
+f_right = codecs.open('thai_new_right.txt' , 'r' ,'utf-8')
+wrongF1 = codecs.open('thai_new_cut_wrong_cut2right.txt' , 'w' , 'utf-8')
+wrongF2 = codecs.open('thai_new_cut_wrong_right2cut.txt' , 'w' , 'utf-8')
+right_word = []
+for line in f_right.readlines():
+    word = line.strip()
+    if word != '':
+        right_word.append(word)
+    
+
+allWordCount = len(right_word)
+rightWordCount1 = 0
+for w in cut_result:
+    if w in right_word:
+        rightWordCount1 += 1
+    else:
+        print w
+        wrongF1.write(w + '\n')
+        
+        
+rightWordCount2 = 0
+for w in right_word:
+    if w in cut_result:
+        rightWordCount2 += 1
+    else:
+        print w
+        wrongF2.write(w + '\n')
+
+f_right.close()
+wrongF1.close()
+wrongF2.close()
+print 'The correct rate (cut->right) is %.2f%%' % float(rightWordCount1*100.0/allWordCount)
+print 'The news has %d words' % allWordCount
+print 'The thaicut tool cut %d words' % len(cut_result)
+print 'The news has %d right (cut->right) cuted words' % rightWordCount1
+print '\n'
+print 'The correct rate (right2cut) is %.2f%%' % float(rightWordCount2*100.0/allWordCount)
+print 'The news has %d words' % allWordCount
+print 'The thaicut tool cut %d words' % len(cut_result)
+print 'The news has %d right (right2cut) cuted words' % rightWordCount2
